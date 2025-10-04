0bb0ebd752aa340ccdf1cd5b1460e9d629fae600
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-05 00:44:46 +0700

    feat: add icons and images constants for asset management

---
+++ src/constant/images.js
@@ -0,0 +1,31 @@
+import Image1 from '@/assets/images/image1.png'
+import Image2 from '@/assets/images/image2.jpg'
+import Image3 from '@/assets/images/image3.jpg'
+import Image4 from '@/assets/images/image4.png'
+import Image5 from '@/assets/images/image5.jpg'
+import Image6 from '@/assets/images/image6.jpg'
+import Image7 from '@/assets/images/image7.png'
+import Image8 from '@/assets/images/image8.jpg'
+import Image9 from '@/assets/images/image9.png'
+import Image10 from '@/assets/images/image10.png'
+import Image11 from '@/assets/images/image11.jpg'
+import Image12 from '@/assets/images/image12.jpg'
+import Image13 from '@/assets/images/image13.jpg'
+import Image14 from '@/assets/images/image14.jpg'
+
+export const Images = {
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+  Image6,
+  Image7,
+  Image8,
+  Image9,
+  Image10,
+  Image11,
+  Image12,
+  Image13,
+  Image14,
+}
