cc0980c4c8eaa6d1100bfebe2a7d78e833be7435
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-05 07:47:49 +0700

    feat: implement ContainerCardBlockLinks component to display a list of linked cards with images

---
+++ src/components/ContainerCardBlockLinks.vue
@@ -0,0 +1,31 @@
+<script setup>
+import CardBlockLinks from './CardBlockLinks.vue'
+import { Images } from '@/constant/images'
+
+const link =
+  'https://support.google.com/?utm_source=about.google&utm_medium=referral&utm_campaign=productspage'
+
+const CardBlockLinksData = [
+  {
+    title: 'Dukungan Product',
+    link: link,
+    image: Images.Image1,
+  },
+  {
+    title: 'Google Store',
+    link: link,
+    image: Images.Image2,
+  },
+  {
+    title: 'Akun Google Anda',
+    link: link,
+    image: Images.Image3,
+  },
+]
+</script>
+
+<template>
+  <div class="flex flex-col gap-10 items-center justify-center sm:flex-row sm:gap-20 pt-6 sm:pt-12">
+    <CardBlockLinks v-for="card in CardBlockLinksData" :key="card.title" v-bind="card" />
+  </div>
+</template>
