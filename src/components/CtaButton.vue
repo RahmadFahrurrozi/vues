83f45ab57a020849d687c963d20796ec4da0d80f
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-05 08:01:13 +0700

    feat: add CtaButton component for enhanced call-to-action functionality

---
+++ src/components/CtaButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="px-8 py-3 bg-blue-100 text-blue-700 font-semibold rounded-full cursor-pointer hover:shadow-md transition duration-300 ease-in-out"
+  >
+    <slot></slot>
+  </button>
+</template>
