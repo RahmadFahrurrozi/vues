9fc35aba8b3a6b38397347a1b7581bb69a92ba36
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-05 08:01:39 +0700

    feat: add CtaButton to AboutProductView for improved user navigation

--- src/views/AboutproductView.vue
+++ src/views/AboutproductView.vue
@@ -2,10 +2,16 @@
 import ContainerCardBlockLinks from '@/components/ContainerCardBlockLinks.vue'
 import HeroSection from '@/components/HeroSection.vue'
 import MarQueeLogos from '@/components/MarQueeLogos.vue'
+import CtaButton from '@/components/CtaButton.vue'
 </script>
 
 <template>
-  <HeroSection />
-  <MarQueeLogos />
-  <ContainerCardBlockLinks />
+  <section>
+    <HeroSection />
+    <MarQueeLogos />
+    <ContainerCardBlockLinks />
+    <div class="flex justify-center py-12">
+      <CtaButton>Lewati ke produk kami</CtaButton>
+    </div>
+  </section>
 </template>
