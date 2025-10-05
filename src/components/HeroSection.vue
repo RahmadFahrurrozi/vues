3f087890d8c5ab0a3630f36758886e5b9d7de27e
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-05 08:01:27 +0700

    fix: adjust minimum height of HeroSection container for improved layout

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,7 +2,7 @@
 
 <template>
   <section class="flex justify-center items-center flex-col">
-    <div class="container flex justify-center items-center min-h-[40vh]">
+    <div class="container flex justify-center items-center min-h-[35vh]">
       <h1 class="text-6xl text-center leading-tight font-medium">
         Produk bermanfaat yang dibuat dengan mengutamakan kebutuhan Anda
       </h1>
