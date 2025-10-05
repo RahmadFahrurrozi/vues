1c6c1c850a6d17c3e572b6394d64bae9c98fa7ff
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-05 07:47:34 +0700

    feat: add CardBlockLinks and ContainerCardBlockLinks components for displaying linked cards

--- src/components/AboutProduct.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-  <div>
-    <h1 class="text-2xl font-bold text-emerald-500">About Product</h1>
-  </div>
-</template>
---
+++ src/components/CardBlockLinks.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ArrowRight } from 'lucide-vue-next'
+
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    default: '#',
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <a :href="link">
+    <div class="flex border border-gray-200 rounded-md max-w-sm cursor-pointer">
+      <div class="w-[420px] flex flex-col justify-around pl-4">
+        <h3 class="text-gray-800 font-semibold">
+          {{ title }}
+        </h3>
+        <a :href="link">
+          <ArrowRight class="w-6 h-6 text-blue-500" />
+        </a>
+      </div>
+      <div class="w-1/2">
+        <img :src="image" alt="image" class="rounded-e-md object-cover h-48 w-full" />
+      </div>
+    </div>
+  </a>
+</template>
