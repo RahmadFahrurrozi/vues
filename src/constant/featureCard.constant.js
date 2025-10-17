515bfbfde709f96cc762052f30d8abdf967a3226
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-17 10:44:20 +0700

    feat: create data feature card

---
+++ src/constant/featureCard.constant.js
@@ -0,0 +1,25 @@
+import { Images } from './images.constant'
+
+export const FEATURE_CARD_DATA1 = [
+  {
+    title: 'Lingkari untuk Menelusuri',
+    description:
+      'Telusuri apa pun di ponsel Android Anda dengan gestur sederhana — seperti melingkari, menandai, mencoret-coret, atau mengetuk.',
+    image: Images.Image5,
+    link: 'https://example.com/lingkari',
+  },
+  {
+    title: 'Gemini untuk Google Workspace',
+    description:
+      'Jadi lebih produktif dengan Gemini, yang terintegrasi langsung dengan Gmail, Dokumen, Spreadsheet, dan lainnya.',
+    image: Images.Image6,
+    link: 'https://example.com/gemini',
+  },
+  {
+    title: 'Immersive View',
+    description:
+      'Jelajahi tempat secara virtual di Maps, dengan detail seperti cuaca dan tingkat kesibukan sebagai perkiraan keadaan.',
+    image: Images.Image7,
+    link: 'https://example.com/maps',
+  },
+]
