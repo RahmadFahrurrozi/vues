bd3214cbfb4acacea10453916ce68b85a78079f6
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-17 10:43:46 +0700

    feat: create feature link constant link

---
+++ src/constant/featureLinkSection.constant.js
@@ -0,0 +1,5 @@
+export const FEATURE_SECTION_LINK = {
+  SEARCH:
+    'https://www.google.com/search/howsearchworks/?utm_source=about.google&utm_medium=referral&utm_campaign=productspage',
+  GEMINI: 'https://gemini.google.com/',
+}
