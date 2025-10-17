cacebf81ad6c2dc2a273d5a1e41d35e193e96792
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-17 10:49:23 +0700

    fix: correct import constant icon and improve some ui animation

--- src/components/MarQueeLogos.vue
+++ src/components/MarQueeLogos.vue
@@ -1,13 +1,19 @@
 <script setup>
-import { icons } from '@/constant/icons'
+import { icons } from '@/constant/icons.constant'
 </script>
 
 <template>
-  <div class="overflow-hidden whitespace-nowrap pb-6">
+  <div class="relative overflow-hidden whitespace-nowrap pb-6">
     <div class="flex items-center gap-14 animate-marquee">
       <img v-for="(logo, index) in icons" :key="index" :src="logo" alt="logo" class="h-9 w-auto" />
       <img v-for="(logo, index) in icons" :key="index" :src="logo" alt="logo" class="h-9 w-auto" />
     </div>
+    <div
+      class="pointer-events-none absolute left-0 top-0 h-full w-3xs bg-gradient-to-r from-white to-transparent"
+    ></div>
+    <div
+      class="pointer-events-none absolute right-0 top-0 h-full w-3xs bg-gradient-to-l from-white to-transparent"
+    ></div>
   </div>
 </template>
 
