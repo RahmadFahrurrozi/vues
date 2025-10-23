de87004d095a10253cba891b303a2e657e7509ae
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-23 07:38:56 +0700

    feat: add FoollowInformation component for product and company updates

---
+++ src/components/FoollowInformation.vue
@@ -0,0 +1,18 @@
+<script setup>
+import CtaButton from './CtaButton.vue'
+</script>
+
+<template>
+  <div class="flex justify-center flex-col items-center py-20 px-4">
+    <h2 class="text-6xl text-center max-w-7xl">
+      Ikuti informasi terbaru tentang produk dan perusahaan kami
+    </h2>
+    <a
+      href="https://blog.google/?utm_source=about.google&utm_medium=referral&utm_campaign=homepagehero"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <cta-button class="my-8">Mulai</cta-button>
+    </a>
+  </div>
+</template>
