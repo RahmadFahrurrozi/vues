6f2fa48a57828ef41e255c3303c4a90e3c444408
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-23 07:38:44 +0700

    feat: add HomeBanner component with image for the homepage

---
+++ src/components/HomeBanner.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="min-w-full pt-4">
+    <img class="w-full object-cover" src="../assets/images/beranda/image1.png" alt="home-banner" />
+  </div>
+</template>
