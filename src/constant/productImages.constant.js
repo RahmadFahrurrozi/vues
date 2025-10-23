a82655476aeccc7d69f88bc9180bf256df7fe159
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-23 07:36:18 +0700

    feat: update font and add new images for beranda and produk sections; remove old image constants

---
+++ src/constant/productImages.constant.js
@@ -0,0 +1,31 @@
+import Image1 from '@/assets/images/produk/image1.png'
+import Image2 from '@/assets/images/produk/image2.jpg'
+import Image3 from '@/assets/images/produk/image3.jpg'
+import Image4 from '@/assets/images/produk/image4.png'
+import Image5 from '@/assets/images/produk/image5.jpg'
+import Image6 from '@/assets/images/produk/image6.jpg'
+import Image7 from '@/assets/images/produk/image7.png'
+import Image8 from '@/assets/images/produk/image8.jpg'
+import Image9 from '@/assets/images/produk/image9.png'
+import Image10 from '@/assets/images/produk/image10.png'
+import Image11 from '@/assets/images/produk/image11.jpg'
+import Image12 from '@/assets/images/produk/image12.jpg'
+import Image13 from '@/assets/images/produk/image13.jpg'
+import Image14 from '@/assets/images/produk/image14.jpg'
+
+export const Images = {
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+  Image6,
+  Image7,
+  Image8,
+  Image9,
+  Image10,
+  Image11,
+  Image12,
+  Image13,
+  Image14,
+}
